e3899fdbb65e3a05e567c73cb5c254679feb4fda
sharath-devops7 <sharathgsr7@gmail.com>
2023-06-13 15:05:24 +0530

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,17 @@
+<<<<<<< HEAD
 IAM IN THE MASTER BRANCH
 
+=======
+>>>>>>> bb3c90a ( first commit)
 it contains shell scripting
 
 
  i need to practise the shell scripting topic
 
+<<<<<<< HEAD
 iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii    it is very important
 iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
+=======
+    it is very important
+
+>>>>>>> bb3c90a ( first commit)
